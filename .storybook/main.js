6b7009d267ae1a34337f40d98a145ddbd7d81dfe
Robert Schiriac <robert_schiriac@trimble.com>
2023-02-27 09:32:02 +0200

    upgraded sb@next(fixes @mdx-js/react issue) + fixed staticDir issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  staticDir: ['../public'],
+  docs: {
+    autodocs: true
   }
-}
+};
