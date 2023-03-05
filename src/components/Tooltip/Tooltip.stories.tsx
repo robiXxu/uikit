330327adade3d34caddee2e9048190d3822da557
Robert Schiriac <robert_schiriac@trimble.com>
2023-03-05 16:57:29 +0200

    updated
    - using tailwind output.css in .storybook/preview.js
    - added script to actually generate tailwind output.css
    - added concurrently to run both watch:tailwind & watch:storybook
    - fixed Tooltip (supporting all directions)

--- src/components/Tooltip/Tooltip.stories.tsx
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -1,21 +1,29 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { Button } from "../Button/Button";
 import { Tooltip } from "./Tooltip";
 
 export default {
   component: Tooltip,
   argTypes: {
     content: { control: { type: "text" } },
+    direction: {
+      control: { type: "radio" },
+      options: [
+        "top",
+        "bottom",
+        "left",
+        "right",
+      ]
+    },
   },
   args: {
-    content: "Some tooltip text",
+    content: "Tooltip Text Content",
+    children: Button({label: "Testing Tooltip"}),
+    direction: "top"
   },
   decorators: [
-    (Story, context) => (
-      <Story content={context.args.content}>
-        <div>WAFUD</div>
-      </Story>
-    ),
-  ],
+    (Story, content) => <div className="w-full h-full flex justify-center my-14"><Story direction={content.args.direction} content={content.args.content} /></div>
+  ]
 } as Meta<typeof Tooltip>;
 
 type Story = StoryObj<typeof Tooltip>;
--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,18 +1,113 @@
 import { ReactNode, useState } from "react";
 
-export const Tooltip = (props: {
-  children: ReactNode
+const defaultClasses = [
+  "absolute",
+  "rounded-xl",
+  "p-2",
+  "bg-gray-900",
+  "z-50",
+  "font-bold",
+  "text-sky-100",
+  "text-xs",
+  "w-auto",
+  "h-auto",
+  "whitespace-nowrap",
+  "before:absolute",
+  "before:content-[' ']",
+  "before:border-x-transparent",
+  "before:border-y-transparent",
+  "before:w-0",
+  "before:h-0",
+  "before:pointer-events-none",
+  "before:border-8",
+];
+
+const right = [
+  "ml-2",
+  "left-full",
+  "top-1/2",
+  "-translate-y-1/2",
+  "before:left-0",
+  "before:top-1/2",
+  "before:-translate-y-1/2",
+  "before:border-r-gray-900",
+  "before:-ml-4",
+];
+
+const left = [
+  "mr-2",
+  "right-full",
+  "top-1/2",
+  "-translate-y-1/2",
+  "before:right-0",
+  "before:top-1/2",
+  "before:-translate-y-1/2",
+  "before:border-l-gray-900",
+  "before:-mr-4",
+];
+
+const bottom = [
+  "mt-2",
+  "left-1/2",
+  "-translate-x-1/2",
+  "before:top-0",
+  "before:left-1/2",
+  "before:-translate-x-1/2",
+  "before:border-b-gray-900",
+  "before:-mt-4",
+];
+
+const top = [
+  "bottom-full",
+  "mb-2",
+  "left-1/2",
+  "-translate-x-1/2",
+  "before:top-full",
+  "before:left-1/2",
+  "before:-translate-x-1/2",
+  "before:border-t-gray-900",
+  "before:mt-0",
+];
+
+const getDirectionClasses = (
+  direction: TooltipDirection 
+) => {
+  switch (direction) {
+    case "top":
+      return top;
+    case "bottom":
+      return bottom;
+    case "left":
+      return left;
+    case "right":
+      return right;
+    default:
+      return top;
+  }
+};
+
+export type TooltipDirection = "top" | "bottom" | "left" | "right";
+
+export const Tooltip = ({
+  children,
+  content,
+  direction = "top",
+}: {
+  children: ReactNode;
   content: string;
+  direction?: TooltipDirection;
 }) => {
   const [active, setActive] = useState(false);
+  const tooltipClasses = [...defaultClasses, ...getDirectionClasses(direction)].join(" ");
+
   return (
     <div
-      className="inline-block relative"
+      className="relative overflow-visible"
       onMouseEnter={() => setActive(true)}
       onMouseLeave={() => setActive(false)}
     >
-      {props.children}
-      {active && <div>{props.content}</div>}
+      {children}
+      {active && content?.length > 0 && <div className={tooltipClasses}>{content}</div>}
     </div>
   );
 };
